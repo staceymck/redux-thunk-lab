8d02cfff796c8b3e62a955c9e32724570c6a5118
Stacey McKnight <mcknight.stm@gmail.com>
2021-04-20 15:36:37 -0500

    commit tree bfa1523b3cc3b79ab269590556f675b3afc7837e

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,12 @@
-// write your CatList component here
+import React from 'react'
+
+function CatList({catPics}) {
+  console.log(catPics)
+  return (
+    <div>
+      {catPics.map(cat => <img key={cat.id} src={cat.url} />)}
+    </div> 
+  )
+}
+
+export default CatList
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,10 @@
-// write and export your action creator function here
+export const fetchCats = () => {
+  return (dispatch) => {
+    dispatch({type: "LOADING_CATS"})
+    fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+    .then(res => res.json())
+    .then(data => {
+      dispatch({type: "ADD_CATS", cats: data.images})
+    })
+  }
+}
